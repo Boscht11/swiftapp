01a4bde5beb1da29121c692777eb810000687aab
Tristan Bosch <TristanBosch@tristans-mbp-2>
2017-02-06 22:27:01 -0600

    Initial Commit

---
+++ Trial 1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Trial 1
+//
+//  Created by Tristan Bosch on 2/6/17.
+//  Copyright © 2017 Tristan Bosch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
